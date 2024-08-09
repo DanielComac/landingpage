e1d0321516bd1fb14a85ecba53a4eab454e7d4de
DanielComac <danielcomacxbox@gmail.com>
2024-08-09 12:45:01 -0600

    v2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/landingpage/', // Asegúrate de que coincida con el nombre del repositorio
+
 })
